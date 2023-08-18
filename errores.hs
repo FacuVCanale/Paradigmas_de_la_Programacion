6062492bf03d1214661a413859bcb1d93c8c9ac1
FacuVCanale <facundovcanale@gmail.com>
2023-08-18 10:46:32 -0300

    Cosas de clases

---
+++ errores.hs
@@ -0,0 +1,20 @@
+import Control.Exception
+import System.IO.Unsafe
+import Text.XHtml (action)
+
+fallo :: (a -> IO b) -> a -> IO Bool
+fallo function action = do
+    result <- tryJust isException (function action)
+    return $ case result of
+        Left _ -> True
+        Right _ -> False
+    where
+        isException :: SomeException -> Maybe ()
+        isException _ = Just ()
+
+testF :: IO Bool -> Bool
+testF action = unsafePerformIO action
+
+result :: Int -> Int
+result x | x > 5 = 4
+         | otherwise = error "hey"
