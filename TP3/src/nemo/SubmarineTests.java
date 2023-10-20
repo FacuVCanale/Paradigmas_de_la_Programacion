db2bd05537e9b8801b6311af522aaaa23ec2f53c
Juanfra <djfran774@gmail.com>
2023-10-20 13:35:22 -0300

    TP3 finished - Changed Test name to SubmarineTests

--- TP3/src/nemo/SubmarineTests.java
+++ TP3/src/nemo/SubmarineTests.java
@@ -9,7 +9,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-public class Tests {
+public class SubmarineTests {
     private Position origin;
     private Orientation north;
     private Submarine nemo;
